3d0a4660581aa949c3888f2e8b22d975e7c70691
Jason Theophilus <jasontheophilussurjadi@gmail.com>
2022-05-03 20:12:44 +0700

    added ui

--- script.js
+++ script.js
@@ -1,23 +1,22 @@
 function computerPlay() {
     let id = Math.random() * 2;
     id = Math.round(id);
-    if(id == 0) {
+    if (id == 0) {
         computer = "rock";
     }
-    else if(id == 1) {
+    else if (id == 1) {
         computer = "paper";
     }
-    else if(id == 2) {
+    else if (id == 2) {
         computer = "scissors";
     }
     else {
         computer = "error";
     }
-    console.log(id);
     return computer;
 }
 
-function round(playerSelection, computerSelection){
+function round(playerSelection, computerSelection) {
     if (playerSelection === "paper") {
         if (computerSelection === "paper") {
             return "tie";
@@ -76,36 +75,62 @@ let computerRound = 0;
 let keepgoing = true;
 let roundNo = 0;
 
-for (; keepgoing == true; ) {
-    if (playerRound == 5 || computerRound == 5 ) {
+let computerSelection = computerPlay();
+let result = "inot";
+let playerSelection = "inot"
+
+function checkresult(playerRound, computerRound) {
+    if (playerRound == 5 || computerRound == 5) {
         keepgoing = false;
         if (playerRound > computerRound) {
-            console.log("player wins");
+            let newline = document.createElement('h1');
+            newline.textContent = "PLAYER WINS"
+            results.appendChild(newline);
         }
         else {
-            console.log("computer wins");
+            let newline = document.createElement('h1');
+            newline.textContent = "COMPUTER WINS"
+            results.appendChild(newline);
         }
     }
     else {
         keepgoing = true;
-        let playerSelection = window.prompt("rock/paper/scissors");
-        let computerSelection = computerPlay();
-        let result = round(playerSelection, computerSelection);
-        if (result == "win") {
-            playerRound = ++playerRound;
-            roundNo = ++roundNo;
-        }
-        else if (result == "tie") {
-            roundNo = ++roundNo;
-        }
-        else {
-            computerRound = ++computerRound;
-            roundNo = ++roundNo;
-        }
-        console.log("round No: " + roundNo);
-        console.log("player = " + playerSelection);
-        console.log("computer = " + computerSelection);
-        console.log("result = " + result);
-        console.log("player current score: " + playerRound + " computer score: " + computerRound);
     }
 }
+
+buttons = document.querySelectorAll('.input')
+results = document.querySelector('#results')
+
+//    let result = round(playerSelection, computerSelection);
+function roundcount(result) {
+    if (result == "win") {
+        playerRound = ++playerRound;
+        roundNo = ++roundNo;
+    }
+    else if (result == "tie") {
+        roundNo = ++roundNo;
+    }
+    else {
+        computerRound = ++computerRound;
+        roundNo = ++roundNo;
+    }
+    let newline = document.createElement('p');
+    newline.textContent = (
+        "Round No: " + roundNo + "\r\nplayer = " + playerSelection +
+        "\r\ncomputer = " + computerSelection +
+        "\r\nresult = " + result +
+        "\r\nplayer current score: " + playerRound + "\r\ncomputer score: " + computerRound
+    )
+    newline.setAttribute('style', 'white-space: pre;');
+    results.appendChild(newline);
+}
+
+buttons.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        console.log(btn.id)
+        playerSelection = btn.id;
+        result = round(btn.id, computerPlay())
+        roundcount(result);
+        checkresult(playerRound, computerRound);
+    })
+})
